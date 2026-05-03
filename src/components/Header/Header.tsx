6ea6908271a2bf936055486dced443b107971ab7
Mikhail Karasev <seplaz38@yandex.ru>
2026-05-03 18:57:24 +0800

    Style fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,9 @@ import { Logo } from '../Logo/Logo';
 export const Header = () => {
   return (
     <header className={styles.header}>
-      {/* <div className={styles.header__left}></div> */}
+      <div className={styles.header__left}></div>
       <Logo />
-      {/* <div className={styles.header__right}></div> */}
+      <div className={styles.header__right}></div>
     </header>
   );
 };
