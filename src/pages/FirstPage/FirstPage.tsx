897ee994e70e1c83422880142ad2c2c846350026
Mikhail Karasev <seplaz38@yandex.ru>
2026-05-07 20:13:33 +0800

    Refactor FirstPage component to use dynamic phrases; improve readability and maintainability

--- src/pages/FirstPage/FirstPage.tsx
+++ src/pages/FirstPage/FirstPage.tsx
@@ -1,34 +1,58 @@
-import styles from './FirstPage.module.css';
-import { Header } from '../../components/Header/Header';
-import { Title } from '../../components/Title/Title';
-import { Subtitle } from '../../components/Subtitle/Subtitle';
-import arrowDown from '/icons/arrow_down.svg';
+import styles from "./FirstPage.module.css";
+import { Header } from "../../components/Header/Header";
+import { Title } from "../../components/Title/Title";
+import { Subtitle } from "../../components/Subtitle/Subtitle";
+import arrowDown from "/icons/arrow_down.svg";
+import { useState } from "react";
 
 type Props = { active: boolean };
 
+const phrases = [
+  {
+    title: "ПРЕВОСХОДНОЕ ВЛАДЕНИЕ ЯЗЫКОМ",
+    subtitle: "Искусство, от которого сводит ноги",
+  },
+  {
+    title: "МАСТЕРСТВО ГЛУБОКОЙ ЛИНГВИСТИКИ",
+    subtitle: "Глубокий профессионалитет",
+  },
+  {
+    title: "ПРОФЕССОР КЛИТОРАЛЬНОЙ ЛИНГВИСТИКИ",
+    subtitle: "Доктор наук по женскому удовольствию",
+  },
+  {
+    title: "КУНИЛИНГУС КАК РЕЛИГИЯ",
+    subtitle: "Присоединяйся к культу наслаждения",
+  },
+  { title: "АБСОЛЮТНОЕ ВЛАДЕНИЕ ЯЗЫКОМ", subtitle: "Мастерство без границ" },
+  { title: "ВЕЛИКИЙ МОГУЧИЙ ЯЗЫК", subtitle: "Клитор есть — ума не надо" },
+];
+
 export const FirstPage = ({ active }: Props) => {
+  const [currentPhraseIndex] = useState(() =>
+    Math.floor(Math.random() * phrases.length),
+  );
+
+  const phrase = phrases[currentPhraseIndex];
+
   const baseDelayMs = 200;
-  const title = 'ПРЕВОСХОДНОЕ ВЛАДЕНИЕ ЯЗЫКОМ';
-  const wordsCount = title.trim().split(/\s+/).filter(Boolean).length;
+  const wordsCount = phrase.title.trim().split(/\s+/).filter(Boolean).length;
   const subtitleDelayMs = baseDelayMs + (wordsCount - 1) * 250 + 250;
 
   return (
-    <div className={styles.page} data-active={active ? 'true' : 'false'}>
+    <div className={styles.page} data-active={active ? "true" : "false"}>
       <Header />
       <div className={styles.content}>
         <div className={styles.text}>
-          <Title title={title} stackedWords baseDelayMs={baseDelayMs} />
-          <Subtitle
-            subtitle='Искусство, от которого сводит ноги'
-            delayMs={subtitleDelayMs}
-          />
+          <Title title={phrase.title} stackedWords baseDelayMs={baseDelayMs} />
+          <Subtitle subtitle={phrase.subtitle} delayMs={subtitleDelayMs} />
         </div>
       </div>
       <img
         className={styles.arrow}
         src={arrowDown}
-        alt='Вниз'
-        style={{ '--arrow-delay': '1450ms' } as React.CSSProperties}
+        alt="Вниз"
+        style={{ "--arrow-delay": "1450ms" } as React.CSSProperties}
       />
     </div>
   );
